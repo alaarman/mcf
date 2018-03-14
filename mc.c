d24f997768edf5c353b6ccf196dbb520b5b920f3
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2018-03-14 21:21:15 +0100

    mc in C (no longer c++)

--- mc.c
+++ mc.c
@@ -6,76 +6,64 @@
  * (previously called 'greybox', hence the mix of GB* / pins_* functions)
  */
 
-#include <iostream>
-#include <set>
-#include <stack>
-#include <vector>
-
+#include <stdbool.h>
 #include <unistd.h>
 
-extern "C" {
 #include "loader/loader.h"
 #include "pins/pins.h"
 #include "pins/pins-util.h"
 #include "pins2pins/por.h"
+#include "util/dfs-stack.h"
 #include "util/runtime.h"
 #include "util/util.h"
-}
+#include "util-mc/tree-ll.h"
 
-using namespace std;
-
+static size_t TABLE_SIZE = 28; // log2 size of tree table
 static size_t progress = 10000;
 
-class Search {
-  public:
-    // Model
-    model_t model = NULL;
+typedef struct seach_s {
+    model_t model;
     size_t state_size_int;
 
-    // Search
-    set< vector<int> >    visited;
-    stack< vector<int> >  stacked;
+    dfs_stack_t *stack;		// stack
+    tree_ll_t *visited;		// visited set
     size_t states;
     size_t trans;
     size_t deadlocks;
-};
+} search_t;
 
-void print_state(Search *S, int *state) {
-    for(size_t i = 0; i < pins_get_state_label_count(S->model); i++)
-        printf("%d, ", state[i]);
-    printf("\n");
+static inline void
+monitor_progress(search_t *S) {
+	S->states++;
+	if (S->states == progress) {
+		Print("States explored: %zu, transitions: %zu", S->states, S->trans);
+		progress <<= 1; // times 2
+	}
 }
 
 /**
  * Callback processing successors
  */
 void process_cb(void *ctx, transition_info_t *ti, int *dst, int *cpy) {
-    Search *S = (Search *) ctx;
+	search_t *S = (search_t *) ctx;
     S->trans++;
-    vector<int> state = vector<int>(dst, dst + S->state_size_int);
-    S->stacked.push(state);
+    int res = tree_ll_fop (S->visited, dst, true);
+    if (res == DB_ROOTS_FULL || res == DB_LEAFS_FULL)
+    		Abort ("Tree table full, enlarge TABLE_SIZE");
+    if (res == 0) { 						// vector is new
+    		dfs_stack_push (S->stack, dst);	// add vector to the stack
+    }
 }
+
 
 /**
  * Perform search
  */
-void search(Search *S){
-    while(!S->stacked.empty()){
-        vector<int> cur = S->stacked.top();
-        S->stacked.pop();
-
-        // check if already visited
-        if(S->visited.find(cur) != S->visited.end())
-            continue;
-        S->visited.insert(cur);
-
-        S->states++;
-        if (S->states == progress) {
-            std::cout << "States explored: " << S->states << ", transitions: "<< S->trans << endl;
-            progress <<= 1; // times 2
-        }
-        size_t count = GBgetTransitionsAll(S->model, &cur[0],
-                                           process_cb, (void *) S);
+void search(search_t *S){
+    while (dfs_stack_size(S->stack) > 0) {
+		monitor_progress(S);
+        int *cur = dfs_stack_pop(S->stack);
+        size_t count = GBgetTransitionsAll(S->model, cur, process_cb, (void *) S);
         S->deadlocks += count == 0;
     }
 }
@@ -105,32 +93,39 @@ int main(int argc, const char **argv) {
     GBsetChunkMap (model, simple_table_factory_create());
     pins_model_loader (model, fname);
     if (POR) {
+    		Print("Activating Partial Order Reduction layer");
         model = pins2pins_por(model); // wrap model
     }
 
 
     // Print model info:
     size_t l = pins_get_state_variable_count(model);
-    std::cout << "State vectors are " << l << " * 32-bit long" << endl;
-    std::cout << "State slots are: " << endl << "\t";
+    Print("State vectors are %zu * 32-bit long", l);
+    Print("State slots are:");
+    Print0("\t");
     for (size_t i = 0 ; i < l; i++)
-        std::cout << pins_get_state_name(model, i) << ", ";
-    std::cout << endl << endl;
-    std::cout << "There are " << pins_get_state_label_count(model) << " state labels" << endl;
-    std::cout << "State labels are (skipping guards): " << endl << "\t";
+    		Print0("%s,", pins_get_state_name(model, i));
+    Print(" ");
+    Print("There are %zu state labels", pins_get_state_label_count(model));
+    Print("State labels are (skipping guards): ");
+    Print0("\t");
     for (size_t i = 0 ; i <  pins_get_state_label_count(model); i++)
         if (strncmp(pins_get_state_label_name(model, i), "guard", 5) != 0)
-            std::cout << pins_get_state_label_name(model, i) << ", ";
-    std::cout << endl << endl;
+            Print0("%s,", pins_get_state_label_name(model, i));
+    Print(" ");
 
     // setup search
-    Search S;
+    search_t S;
     S.model = model;
     S.state_size_int = l;
     S.states = S.trans = S.deadlocks = 0;
-    vector<int> initial = vector<int> (S.state_size_int);
-    GBgetInitialState (model, &initial[0]);
-    S.stacked.push(initial);
+    S.stack = dfs_stack_create(l);
+    S.visited = tree_ll_create_sized (l, TABLE_SIZE, 2, 0, false, false);
+
+    int *initial = dfs_stack_push(S.stack, NULL); // reserve space
+    GBgetInitialState (model, initial);			 // place initial state on stack
+    // add initial state to stack:
+    tree_ll_fop (S.visited, initial, true);
 
     // Start search
     rt_timer_t timer = RTcreateTimer();
@@ -138,8 +133,6 @@ int main(int argc, const char **argv) {
     search(&S);
     RTstopTimer(timer);
     RTprintTimer(timer, "\nTotal exploration time");
-    std::cout << "State space has " << S.states << " state, "<<
-                                       S.trans << " transitions, and " <<
-                                       S.deadlocks << " deadlocks"
-                                       << endl;
+    Print("State space has %zu states, %zu transitions, and %zu deadlocks",
+    		S.states, S.trans,  S.deadlocks);
 }
