b7a1955903cc47bbc39cf4c4e9889ed052d765b9
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2018-04-02 23:45:02 +0200

    Add util/io

---
+++ util/io/user.h
@@ -0,0 +1,21 @@
+// -*- tab-width:4 ; indent-tabs-mode:nil -*-
+
+/**
+\file user.h
+\brief Abstract IO library.
+*/
+
+#ifndef HRE_IO_USER_H
+#define HRE_IO_USER_H
+
+
+#include "util/io/archive.h"
+#include "util/io/client-server.h"
+#include "util/io/fifo.h"
+#include "util/io/raf.h"
+#include "util/io/stream.h"
+#include "util/io/struct_io.h"
+#include "util/io/types.h"
+
+#endif
+
