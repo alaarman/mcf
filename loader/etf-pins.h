789c08ada2f47a1c82b68c87527b0b666cd956ef
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2018-04-02 23:50:28 +0200

    Add ETF loader

---
+++ loader/etf-pins.h
@@ -0,0 +1,17 @@
+#ifndef ETF_GREYBOX_H
+#define ETF_GREYBOX_H
+
+/**
+\file etf-greybox.h
+*/
+
+#include <pins/pins.h>
+
+/**
+Load an ETF model.
+*/
+extern void ETFloadModel(model_t model,const char*name);
+
+
+#endif
+
